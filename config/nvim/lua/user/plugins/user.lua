24f59582ac54ff1fab8dbcb423800b28fde74b1d
Philippe Serhal <philippe.serhal@netlify.com>
2024-08-21 16:19:32 -0400

    feat(nvim): add back vim-fugitive

--- config/nvim/lua/user/plugins/user.lua
+++ config/nvim/lua/user/plugins/user.lua
@@ -1,9 +1,19 @@
 return {
-  "kylechui/nvim-surround",
-  version = "*",
-  event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({
-    })
-  end
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+      })
+    end
+  },
+  {
+    "tpope/vim-fugitive",
+    event = "VeryLazy",
+  },
+  {
+    "tpope/vim-rhubarb",
+    event = "VeryLazy",
+  }
 }
